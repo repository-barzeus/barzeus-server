0d17d839f2cb224dc97026de8bf9945a343ac1ac
Douglas <github.barzeus@outlook.com.br>
2025-05-22 14:50:03 -0300

    Add Spring Boot application entry point and configuration

--- src/main/java/br/com/barzeus/api/BarzeusApiApplication.java
+++ src/main/java/br/com/barzeus/api/BarzeusApiApplication.java
@@ -1,4 +1,10 @@
 package br.com.barzeus.api;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BarzeusApiApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(BarzeusApiApplication.class, args);
+    }
 }
